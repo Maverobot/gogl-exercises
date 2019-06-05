8e237b7466c14866702ccbc67ce49b8aaa1ba314
Maverobot <quzhengrobot@gmail.com>
2019-06-05 22:08:03 +0200

    added ex1.8

---
+++ chapter01/exercise1.8/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		// Add https:// as prefix if missing
+		const prefix = "http://"
+		if !strings.HasPrefix(url, prefix) {
+			url = prefix + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		_, err = io.Copy(os.Stdout, resp.Body)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: copying %s: %v\n", url, err)
+			os.Exit(1)
+		}
+
+		err = resp.Body.Close()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: closing %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
